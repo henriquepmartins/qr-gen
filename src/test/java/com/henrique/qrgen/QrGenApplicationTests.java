4f84686b3f1e2b765ace33f24f85ded583aef09f
henrique <henriquepermartins@gmail.com>
2025-04-25 14:03:25 -0300

    first commit

---
+++ src/test/java/com/henrique/qrgen/QrGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.qrgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
